89c8130058eeb12dfb496c15282c0e7b6bd38e54
MuxtorovicH <rusdosmux@gamil.com>
2022-11-04 12:13:57 +0500

    Initial commit

---
+++ src/main/java/uz/mrdos/springsecurity/controller/ReportController.java
@@ -0,0 +1,18 @@
+package uz.mrdos.springsecurity.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/report")
+public class ReportController {
+
+    @GetMapping
+    public HttpEntity<?> ReportController(){
+        return ResponseEntity.ok("Report sent");
+    }
+
+}
